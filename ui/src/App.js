82d2beb78475670e4c35d2ea16b74169cadb4f3d
Max DECKMYN <mdeckmyn@lise.kreizenn.bzh>
2025-03-14 16:12:40 +0100

    Test with website

---
+++ ui/src/App.js
@@ -0,0 +1,18 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Test</h1>
+        <p>du site du mont blanc</p>
+        <li>Max</li>
+        <li>Paul</li>
+        <li>Mathieu</li>
+        <li>Nikitas</li>
+      </header>
+    </div>
+  );
+}
+
+export default App;
